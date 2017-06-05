fb87d91fe4fc36ec78f411a115b5dd467f6f05b6
RosenUrkov <rosen.urkov@gmail.com>
2017-06-05 23:57:50 +0300

    Connecting PostgreSQL

---
+++ BibaldoDictionariesSystem/DictionariesSystem.Models/Users/Badge.cs
@@ -0,0 +1,20 @@
+﻿namespace DictionariesSystem.Models.Users
+{
+    using System.Collections.Generic;
+
+    public class Badge
+    {
+        public Badge()
+        {
+            this.Users = new HashSet<User>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int RequiredContributions { get; set; }  
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
---
+++ BibaldoDictionariesSystem/DictionariesSystem.Models/Users/User.cs
@@ -0,0 +1,22 @@
+﻿namespace DictionariesSystem.Models.Users
+{
+    using System.Collections.Generic;
+
+    public class User
+    {
+        public User()
+        {
+            this.Badges = new HashSet<Badge>();
+        }
+
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Passhash { get; set; }
+
+        public int ContributionsCount { get; set; }
+
+        public virtual ICollection<Badge> Badges { get; set; }
+    }
+}
