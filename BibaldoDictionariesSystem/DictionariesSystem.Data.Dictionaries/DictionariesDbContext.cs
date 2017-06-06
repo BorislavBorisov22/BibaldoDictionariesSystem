38d6eb29d9852d5e4b4604a007c99d0a09ce3442
Martin Kamenov <martykam@abv.bg>
2017-06-06 10:57:01 +0300

    Started doing the models and dbContext in the project.

---
+++ BibaldoDictionariesSystem/DictionariesSystem.Data.Dictionaries/DictionariesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace DictionariesSystem.Data.Dictionaries
+{
+    public class DictionariesDbContext : DbContext
+    {
+        public DictionariesDbContext()
+            : base()
+        {
+
+        }
+    }
+}
