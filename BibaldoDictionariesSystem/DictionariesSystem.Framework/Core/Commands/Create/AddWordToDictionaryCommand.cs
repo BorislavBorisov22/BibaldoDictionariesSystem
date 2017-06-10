9b21f568c0a643f9233ddda601c2d52eba2f2661
Martin Kamenov <martykam@abv.bg>
2017-06-10 16:50:36 +0300

    Added functionality for creating word.

--- BibaldoDictionariesSystem/DictionariesSystem.Framework/Core/Commands/Create/AddWordToDictionaryCommand.cs
+++ BibaldoDictionariesSystem/DictionariesSystem.Framework/Core/Commands/Create/AddWordToDictionaryCommand.cs
@@ -15,17 +15,17 @@ namespace DictionariesSystem.Framework.Core.Commands.Create
     public class AddWordToDictionaryCommand : BaseCommand, ICommand
     {
         public const int NumberOfParameters = 2;
-        private readonly IRepository<Word> repository;
+        private readonly IRepository<Dictionary> dictionaries;
         private readonly IUnitOfWork unitOfWork;
         private readonly IUserProvider user;
 
-        public AddWordToDictionaryCommand(IRepository<Word> repository, IUnitOfWork unitOfWork, IUserProvider user)
+        public AddWordToDictionaryCommand(IRepository<Dictionary> dictionaries, IUnitOfWork unitOfWork, IUserProvider user)
         {
-            Guard.WhenArgument(repository, "repository").IsNull().Throw();
+            Guard.WhenArgument(dictionaries, "dictionaries").IsNull().Throw();
             Guard.WhenArgument(unitOfWork, "unitOfWork").IsNull().Throw();
             Guard.WhenArgument(user, "user").IsNull().Throw();
 
-            this.repository = repository;
+            this.dictionaries = dictionaries;
             this.unitOfWork = unitOfWork;
             this.user = user;
         }
@@ -39,7 +39,61 @@ namespace DictionariesSystem.Framework.Core.Commands.Create
 
         public override string Execute(IList<string> parameters)
         {
-            return base.Execute(parameters);
+            string wordName = parameters[0];
+
+            string dictionaryTitle = parameters[1];
+
+            StringBuilder description = new StringBuilder();
+
+            for (int i = 2; i < parameters.Count; i += 1)
+            {
+                if (i != ParametersCount - 1)
+                {
+                    description.Append(parameters[i] + ' ');
+                }
+                else
+                {
+                    description.Append(parameters[i]);
+                }
+            }
+
+            var dictionary = this.dictionaries.All(d => d.Title == dictionaryTitle).FirstOrDefault();
+
+            Guard.WhenArgument(dictionary, "dictionary").IsNull().Throw();
+
+            var foundWord = dictionary.Words.FirstOrDefault(w => w.Name == wordName);
+
+            Guard.WhenArgument(foundWord, "foundWord").IsNotNull().Throw();
+
+            var foundMeaning = dictionary.Meanings.FirstOrDefault(m => m.Description == description.ToString());
+
+            Word newWord = new Word()
+            {
+                Name = wordName,
+                Dictionary = dictionary,
+                DictionaryId = dictionary.Id
+            };
+
+            if (foundMeaning == null)
+            {
+                var meaning = new Meaning()
+                {
+                    Description = description.ToString()
+                };
+                newWord.Meanings.Add(meaning);
+            }
+            else
+            {
+                newWord.Meanings.Add(foundMeaning);
+            }
+
+            dictionary.Words.Add(newWord);
+
+            this.unitOfWork.SaveChanges();
+
+            string result = $"A new word: {wordName} was added into dictionary: {dictionaryTitle}\n{wordName} means {description}";
+
+            return result;
         }
     }
 }
