7d243a10e0f92a634f75f48266dfbf9408898aa7
RosenUrkov <rosen.urkov@gmail.com>
2017-06-10 12:22:39 +0300

    Added command interface

---
+++ BibaldoDictionariesSystem/DictionariesSystem.Framework/Core/Engine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DictionariesSystem.Contracts.Core;
+
+namespace DictionariesSystem.Framework.Core
+{
+    public class Engine : IEngine
+    {
+        public Engine()
+        {
+                
+        }
+
+        public void Start()
+        {
+            
+        }
+    }
+}
