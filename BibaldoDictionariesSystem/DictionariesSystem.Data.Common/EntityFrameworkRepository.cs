feba3be05da7f4a20f4ee249ab22e8e2b521ae4a
Borislav Borisov <borislavborisov22@abv.bg>
2017-06-10 12:58:49 +0300

    Removed unnecessary space from ConsoleCLient App.config

--- BibaldoDictionariesSystem/DictionariesSystem.Data.Common/EntityFrameworkRepository.cs
+++ BibaldoDictionariesSystem/DictionariesSystem.Data.Common/EntityFrameworkRepository.cs
@@ -1,29 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using DictionariesSystem.Contracts.Data;
+using System.Linq.Expressions;
 
 namespace DictionariesSystem.Data.Common
 {
-    public class Repository<T> : IRepository<T> where T : class
+    public class Repository<T>
     {
-        public void Add(T entity)
-        {
-            
-        }
-
-        public IEnumerable<T> All()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(T entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public T GetById(object id)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
