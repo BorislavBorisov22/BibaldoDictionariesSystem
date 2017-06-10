14d78a324e93d38f2f25c1f4aa1f6b4fb85d8d04
Martin Kamenov <martykam@abv.bg>
2017-06-10 14:03:54 +0300

    Started creating commands starting with creating Dictionary.

---
+++ BibaldoDictionariesSystem/DictionariesSystem.Framework/Core/Commands/CreateDictionaryCommand.cs
@@ -0,0 +1,49 @@
+﻿using DictionariesSystem.Contracts.Core.Commands;
+using DictionariesSystem.Contracts.Data;
+using DictionariesSystem.Models.Dictionaries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionariesSystem.Framework.Core.Commands
+{
+    public class CreateDictionaryCommand : ICommand
+    {
+        private readonly IRepository<Dictionary> repository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public CreateDictionaryCommand(IRepository<Dictionary> repository,IUnitOfWork unitOfWork)
+        {
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+        }
+        public string Execute(IList<string> parameters)
+        {
+            string title = parameters[0];
+
+            string author = parameters[1];
+        
+            string languageName = parameters[2];
+
+            Language language = new Language()
+            {
+                Name = languageName
+            };
+
+            Dictionary dictionary = new Dictionary()
+            {
+                Author = author,
+                Title = title,
+                Language = language
+            };
+
+            this.repository.Add(dictionary);
+
+            string result = $"A ne dictionary with title {title}, author {author} and language {languageName} was created.";
+
+            return result;
+        }
+    }
+}
