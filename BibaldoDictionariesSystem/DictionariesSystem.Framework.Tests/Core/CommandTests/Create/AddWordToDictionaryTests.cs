f15262240d6fc467a54ae7e1372ad0f726e12c32
Martin Kamenov <martykam@abv.bg>
2017-06-11 11:58:09 +0300

    Added framework unit tests for commands.

---
+++ BibaldoDictionariesSystem/DictionariesSystem.Framework.Tests/Core/CommandTests/Create/AddWordToDictionaryTests.cs
@@ -0,0 +1,40 @@
+﻿using DictionariesSystem.Contracts.Core.Factories;
+using DictionariesSystem.Contracts.Core.Providers;
+using DictionariesSystem.Contracts.Data;
+using DictionariesSystem.Framework.Core.Commands.Create;
+using DictionariesSystem.Models.Dictionaries;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionariesSystem.Framework.Tests.Core.CommandTests.Create
+{
+    [TestFixture]
+    public class AddWordToDictionaryTests
+    {
+        [Test]
+        public void ConstructorShould_ThrowWhen_NullRepositoryIsPassed()
+        {
+            var unitOfWork = new Mock<IUnitOfWork>();
+            var userProvider = new Mock<IUserProvider>();
+            var dictionaryFactory = new Mock<IDictionariesFactory>();
+
+            Assert.Throws<ArgumentNullException>(()=>new AddWordToDictionaryCommand(null, unitOfWork.Object, userProvider.Object, dictionaryFactory.Object));
+        }
+
+        [Test]
+        public void ConstructorShould_ThrowWhen_NullUnitOfWorkIsPassed()
+        {
+            var repository = new Mock<IRepository<Dictionary>>();
+            var unitOfWork = new Mock<IUnitOfWork>();
+            var userProvider = new Mock<IUserProvider>();
+            var dictionaryFactory = new Mock<IDictionariesFactory>();
+
+            Assert.Throws<ArgumentNullException>(() => new AddWordToDictionaryCommand(repository.Object, null, userProvider.Object, dictionaryFactory.Object));
+        }
+    }
+}
