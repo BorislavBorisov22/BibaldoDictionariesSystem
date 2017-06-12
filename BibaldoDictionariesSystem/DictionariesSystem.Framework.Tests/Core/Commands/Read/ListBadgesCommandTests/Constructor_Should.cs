5c657d68b1af66dc539ae2aecc3963b4508418d3
Martin Kamenov <martykam@abv.bg>
2017-06-12 19:01:41 +0300

    Added tests for ListBadgesCommand.

---
+++ BibaldoDictionariesSystem/DictionariesSystem.Framework.Tests/Core/Commands/Read/ListBadgesCommandTests/Constructor_Should.cs
@@ -0,0 +1,25 @@
+﻿using DictionariesSystem.Contracts.Core.Providers;
+using DictionariesSystem.Framework.Core.Commands.Read;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace DictionariesSystem.Framework.Tests.Core.Commands.Read.ListBadgesCommandTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void Throw_WhenNoUserProviderIsProvided()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ListBadgesCommand(null));
+        }
+
+        [Test]
+        public void NotThrow_WhenUserProviderIsProvided()
+        {
+            var userProvider = new Mock<IUserProvider>();
+            Assert.DoesNotThrow(() => new ListBadgesCommand(userProvider.Object));
+        }
+    }
+}
---
+++ BibaldoDictionariesSystem/DictionariesSystem.Framework.Tests/Core/Commands/Read/ListBadgesCommandTests/Execute_Should.cs
@@ -0,0 +1,45 @@
+﻿using DictionariesSystem.Contracts.Core.Providers;
+using DictionariesSystem.Framework.Core.Commands.Read;
+using DictionariesSystem.Models.Users;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionariesSystem.Framework.Tests.Core.Commands.Read.ListBadgesCommandTests
+{
+    [TestFixture]
+    public class Execute_Should
+    {
+        [Test]
+        public void ShouldThrow_WhenNoUserIsLoggedIn()
+        {
+            var userProvider = new Mock<IUserProvider>();
+            var command = new ListBadgesCommand(userProvider.Object);
+
+            Assert.Throws<ArgumentException>(() => command.Execute(new List<string>()));
+        }
+
+        [Test]
+        public void ShouldReturnCorrectMessage_WhenTheUserHasNoBadges()
+        {
+            var userProvider = new Mock<IUserProvider>();
+            var command = new ListBadgesCommand(userProvider.Object);
+            var user = new User()
+            {
+                Username = "SomeUsername"
+            };
+
+            userProvider.Setup(u => u.IsLogged).Returns(true);
+            userProvider.Setup(u => u.LoggedUser).Returns(user);
+
+            string result = command.Execute(new List<string>());
+            string expectedResult = $"{userProvider.Object.LoggedUser.Username} has no badges.\r\n";
+
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
