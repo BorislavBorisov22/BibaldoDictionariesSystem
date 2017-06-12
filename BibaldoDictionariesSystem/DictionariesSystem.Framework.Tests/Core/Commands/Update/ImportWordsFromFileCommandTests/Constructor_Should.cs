a59b32288bd7c9488b991c6f4d840d203b21fff7
Borislav Borisov <borislavborisov22@abv.bg>
2017-06-12 19:03:19 +0300

    Added ImportWordsFromFileCommand tests

---
+++ BibaldoDictionariesSystem/DictionariesSystem.Framework.Tests/Core/Commands/Update/ImportWordsFromFileCommandTests/Constructor_Should.cs
@@ -0,0 +1,29 @@
+﻿using DictionariesSystem.Contracts.Core.Factories;
+using DictionariesSystem.Framework.Core.Commands.Update;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace DictionariesSystem.Framework.Tests.Core.Commands.Update.ImportWordsFromFileCommandTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowArgumentNullException_WhenPassedWordsImporterFactoryIsNull()
+        {
+            // arrange and act and assert
+            Assert.Throws<ArgumentNullException>(() => new ImportWordsFromFileCommand(null));
+        }
+
+        [Test]
+        public void NotThrow_WhenPassedArgumentAreValid()
+        {
+            // arrange
+            var importersFactory = new Mock<IWordsImporterFactory>();
+
+            // act and assert
+            Assert.DoesNotThrow(() => new ImportWordsFromFileCommand(importersFactory.Object));
+        }
+    }
+}
---
+++ BibaldoDictionariesSystem/DictionariesSystem.Framework.Tests/Core/Commands/Update/ImportWordsFromFileCommandTests/Execute_Should.cs
@@ -0,0 +1,103 @@
+﻿using DictionariesSystem.Contracts.Core.Factories;
+using DictionariesSystem.Contracts.Loaders;
+using DictionariesSystem.Framework.Core.Commands.Update;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DictionariesSystem.Framework.Tests.Core.Commands.Update.ImportWordsFromFileCommandTests
+{
+    [TestFixture]
+    public class Execute_Should
+    {
+        [Test]
+        public void CallWordsImporterFactoryWithCorrectFileType_WhenPassedParametersAreValid()
+        {
+            // arrange
+            var importersFactory = new Mock<IWordsImporterFactory>();
+
+            string fileType = "SomeFileType";
+            string dictionaryTitle = "SomeTitle";
+            string filePath = "SomePath";
+            var commandParameters = new List<string>()
+            {
+              fileType,
+              dictionaryTitle,
+              filePath
+            };
+
+            var importer = new Mock<IWordsImporterProvider>();
+            importersFactory
+                .Setup(x => x.GetImporter(It.IsAny<string>()))
+                .Returns(importer.Object);
+      
+            var importWordsFromFileCommand = new ImportWordsFromFileCommand(importersFactory.Object);
+
+            // act
+            importWordsFromFileCommand.Execute(commandParameters);
+
+            // assert
+            importersFactory.Verify(x => x.GetImporter(fileType), Times.Once);
+        }
+
+        [Test]
+        public void CallTheTargetImportersImportMethodWithCorrectDictionaryTitleAndFilePath_WhenPassedParametersAreValid()
+        {
+            // arrange
+            var importersFactory = new Mock<IWordsImporterFactory>();
+
+            string fileType = "SomeFileType";
+            string dictionaryTitle = "SomeTitle";
+            string filePath = "SomePath";
+            var commandParameters = new List<string>()
+            {
+              fileType,
+              dictionaryTitle,
+              filePath
+            };
+
+            var importer = new Mock<IWordsImporterProvider>();
+            importersFactory
+                .Setup(x => x.GetImporter(It.IsAny<string>()))
+                .Returns(importer.Object);
+
+            var importWordsFromFileCommand = new ImportWordsFromFileCommand(importersFactory.Object);
+
+            // act
+            importWordsFromFileCommand.Execute(commandParameters);
+
+            // assert
+            importer.Verify(x => x.Import(filePath, dictionaryTitle));
+        }
+
+        [Test]
+        public void ReturnCorrectResultMessage_WhenPassedParametersAreValid()
+        {
+            // arrange
+            var importersFactory = new Mock<IWordsImporterFactory>();
+
+            string fileType = "SomeFileType";
+            string dictionaryTitle = "SomeTitle";
+            string filePath = "SomePath";
+            var commandParameters = new List<string>()
+            {
+              fileType,
+              dictionaryTitle,
+              filePath
+            };
+
+            var importer = new Mock<IWordsImporterProvider>();
+            importersFactory
+                .Setup(x => x.GetImporter(It.IsAny<string>()))
+                .Returns(importer.Object);
+
+            var importWordsFromFileCommand = new ImportWordsFromFileCommand(importersFactory.Object);
+
+            // act
+            string resultMessage = importWordsFromFileCommand.Execute(commandParameters);
+
+            // assert
+            StringAssert.Contains($"from {filePath} have been successfuly imported", resultMessage);
+        }
+    }
+}
